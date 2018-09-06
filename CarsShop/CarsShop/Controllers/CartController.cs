88c0997440987c21a44639930093ef1cc2257c46
TTPi <phongb1505902@student.ctu.edu.vn>
2018-09-06 18:28:59 +0700

    updateCode

---
+++ CarsShop/CarsShop/Controllers/CartController.cs
@@ -0,0 +1,60 @@
+﻿using CarsShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarsShop.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Buy(string id)
+        {
+            ProductModel productModel = new ProductModel();
+            if (Session["cart"] == null)
+            {
+                List<Item> cart = new List<Item>();
+                cart.Add(new Item { Product = productModel.find(id), Quantity = 1 });
+                Session["cart"] = cart;
+            }
+            else
+            {
+                List<Item> cart = (List<Item>)Session["cart"];
+                int index = isExist(id);
+                if (index != -1)
+                {
+                    cart[index].Quantity++;
+                }
+                else
+                {
+                    cart.Add(new Item { Product = productModel.find(id), Quantity = 1 });
+                }
+                Session["cart"] = cart;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Remove(string id)
+        {
+            List<Item> cart = (List<Item>)Session["cart"];
+            int index = isExist(id);
+            cart.RemoveAt(index);
+            Session["cart"] = cart;
+            return RedirectToAction("Index");
+        }
+        private int isExist(string id)
+        {
+            List<Item> cart = (List<Item>)Session["cart"];
+            for (int i = 0; i < cart.Count; i++)
+                if (cart[i].Product.Id.Equals(id))
+                    return i;
+            return -1;
+        }
+    }
+}
---
+++ CarsShop/CarsShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarsShop.MvcApplication" Language="C#" %>
